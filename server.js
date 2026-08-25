d14dc7abaa677aa2f97d841bc295a7585602525d
banjoboy <kuzablo422@yandex.ru>
2026-08-26 02:29:44 +0300

    bump 2.6.5

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { createClient } = require('@supabase/supabase-js');
 const WebSocket = require('ws');
 const multer = require('multer');
 
-const VERSION = '2.6.3';
+const VERSION = '2.6.5';
 const PORT = process.env.PORT || 3000;
 const IDLE_TIMEOUT_MS = 3 * 60 * 1000;
 
